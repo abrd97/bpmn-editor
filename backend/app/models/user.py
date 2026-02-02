910fa575470fb28c7439eb6648223685d318aa70
Aleks <aleks.brd@gmail.com>
2026-02-02 02:39:35 +0100

    update user to automaitcally genertae user uuid

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel, Field
+from uuid import uuid4
 
 
 class User(BaseModel):
     """User model for collaboration"""
-    id: str = Field(..., description="Unique user identifier")
+    id: str = Field(default_factory=lambda: str(uuid4()), description="Unique user identifier")
     name: str = Field(..., description="User display name")
     color: str = Field(..., description="User color for UI in hex format")
